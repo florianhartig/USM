3464182bf2b7616602cf5b031658f4245dd67ac5
Florian Hartig <florianhartig@users.noreply.github.com>
2016-07-04 19:30:36 +0200

    Update install_Rtools.R

--- Code/CIP/install_Rtools.R
+++ Code/CIP/install_Rtools.R
@@ -1,8 +1,34 @@
 install.packages("installr")
+install.packages("devtools")
+
+install.packages("sensitivity")
+
+
 library(installr)
 
-install.Rtools(choose_version = FALSE, check = TRUE, use_GUI = TRUE,
-page_with_download_url = "http://cran.r-project.org/bin/windows/Rtools/")
+# set Rtools 33
+install.Rtools(choose_version = FALSE, check = TRUE, GUI = TRUE)
+
+# Weil wir keine admin Rechte haben müssen wir den Pfad mit Hand setzen
+
+Sys.setenv(PATH = paste('c:\\Rtools\\bin;C:\\Rtools\\gcc-4.6.3\\bin;', Sys.getenv("PATH"), sep = ""))
+
+Sys.getenv("PATH")
 
 
-## make sure you tick the box to change path during the installation
+
+library(devtools)
+
+install.packages("magrittr")
+install_url("https://cran.r-project.org/src/contrib/Archive/RNetLogo/RNetLogo_1.0-1.tar.gz", dependencies =T )
+
+
+# Noch nicht installieren
+
+install_url("https://dl.dropboxusercontent.com/s/ccvpnvblf3vzuvs/BayesianTools_0.0.0.9000.tar.gz", dependencies = T)
+
+library(BayesianTools)
+?BayesianTools
+vignette("AQuickStart", package = "BayesianTools")
+
+install_url("https://dl.dropboxusercontent.com/s/zn64j1tut47v9mj/Rpreles_1.0.tar.gz", dependencies = T)
